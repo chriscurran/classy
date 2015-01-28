b5c4714768180c05f146909c5cb92d806f861fe5
Chris Curran <chris@springshare.com>
2015-01-28 02:31:59 -0500

    config chanegs

--- config.php
+++ config.php
@@ -15,27 +15,13 @@
 	define('DO_STATIC', false);
 
 	//
-	// define db settings based on what machine we're on
+	// define db settings
 	//
-	//set_include_path(get_include_path() .
-	//	PATH_SEPARATOR . "answers/include/common");
-
 	$db_host = "127.0.0.1";
-
-	if (0) {		
-		$db_user = "root";
-		$db_pw 	 = "ads6264";
-		$db_name = "forms";
-		$db_port = 3306;
-	}
-	else {
-		$db_user = "springadmin";
-		$db_pw 	 = "L7EZXhPjauYD2zRn";
-		$db_name = "libapps";
-		$db_name = "libsurveys";
-		$db_name = "api";
-		$db_port = 3309;
-	}
+	$db_user = "root";
+	$db_pw 	 = "ads6264";
+	$db_name = "forms";
+	$db_port = 3306;
 
 	date_default_timezone_set ("UTC");
 ?>
